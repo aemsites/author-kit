4ed1f922930b39a7722a62620c8eff3a3bcc9a39
Chris Millar <cmillar@adobe.com>
2026-01-25 23:00:01 -0700

    Gate all pre-prod tools

--- scripts/lazy.js
+++ scripts/lazy.js
@@ -1,7 +1,34 @@
+import ENV from './utils/env.js';
+
+async function loadSidekick() {
+  const getSk = () => document.querySelector('aem-sidekick');
+
+  const sk = getSk() || await new Promise((resolve) => {
+    document.addEventListener('sidekick-ready', () => resolve(getSk()));
+  });
+  if (sk) import('../tools/sidekick/sidekick.js').then((mod) => mod.default);
+}
+
+async function loadQuickEdit() {
+  const mod = await import('../tools/quick-edit/quick-edit.js');
+  mod.default();
+}
+
 (function loadLazy() {
   import('./utils/lazyhash.js');
   import('./utils/favicon.js');
   import('./utils/footer.js').then(({ default: footer }) => footer());
-  import('../tools/scheduler/scheduler.js');
-  import('../tools/sidekick/sidekick.js');
+
+  // Pre-prod tools
+  if (ENV !== 'prod') {
+    import('../tools/scheduler/scheduler.js');
+    loadSidekick();
+
+    const url = new URL(window.location.href);
+    if (url.searchParams.has('quickedit')) {
+      url.searchParams.delete('quickedit');
+      window.history.replaceState(null, '', url.href);
+      loadQuickEdit();
+    }
+  }
 }());
--- tools/sidekick/sidekick.js
+++ tools/sidekick/sidekick.js
@@ -1,17 +1,8 @@
 import toggleScheduler from '../scheduler/scheduler.js';
 import initQuickEdit from '../quick-edit/quick-edit.js';
 
-const getSk = () => document.querySelector('aem-sidekick');
-
-async function ready(sk) {
+export default async function init(sk) {
   sk.classList.add('is-ready');
   sk.addEventListener('custom:scheduler', toggleScheduler);
   sk.addEventListener('custom:quick-edit', initQuickEdit);
 }
-
-(async function loadSidekick() {
-  const sk = getSk() || await new Promise((resolve) => {
-    document.addEventListener('sidekick-ready', () => resolve(getSk()));
-  });
-  ready(sk);
-}());
