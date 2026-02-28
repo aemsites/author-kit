f0d4873b3f82f1caa551d0652857c066706d28be
Chris Millar <cmillar@adobe.com>
2026-02-27 17:28:58 -0700

    WIP

--- tools/target/target.js
+++ tools/target/target.js
@@ -4,7 +4,22 @@ import loadScript from '../../scripts/utils/script.js';
 const { codeBase } = getConfig();
 
 await (async function target() {
+  window.targetGlobalSettings = {
+    bodyHidingEnabled: false,
+    autoCreate: false
+  };
+
   await loadScript(`${codeBase}/deps/at/at.js`);
+
+  const response = await adobe.target.getOffers({
+    request: {
+      execute: {
+        mboxes: [{ index: 0, name: 'target-global-mbox' }]
+      }
+    }
+  });
+
+  console.log(response);
 
   document.addEventListener('at-request-succeeded', (e) => {
     console.log(e);
