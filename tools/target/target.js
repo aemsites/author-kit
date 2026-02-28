0cbcb547169c65e5828ab6122349bef0237413b8
Chris Millar <cmillar@adobe.com>
2026-02-27 18:36:16 -0700

    Test

--- tools/target/target.js
+++ tools/target/target.js
@@ -9,17 +9,10 @@ const targetFinished = () => {
   loadScript(`${codeBase}/deps/at/at.js`);
 
   return new Promise((resolve) => {
-    // document.addEventListener('at-request-succeeded', (e) => {
-    //   const { response } = e.detail;
-    //   const hasActivities = response?.execute?.pageLoad?.options?.length > 0;
-    //   if (!hasActivities) resolve();
-    // });
-
-
     document.addEventListener(LOAD_EVENT, () => {
       const markers = document.querySelectorAll('.at-element-marker');
       for (const marker of markers) {
-        marker.replaceWith(...marker.childNodes);
+        marker.after(...marker.children);
       }
       resolve();
     });
