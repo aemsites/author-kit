4b15145d494e45985669f139bde3372662ce92a1
Chris Millar <cmillar@adobe.com>
2026-02-27 18:24:18 -0700

    Test

--- tools/target/target.js
+++ tools/target/target.js
@@ -8,13 +8,14 @@ const LOAD_EVENT = 'at-content-rendering-succeeded';
 const targetFinished = () => {
   loadScript(`${codeBase}/deps/at/at.js`);
 
-  document.addEventListener('at-request-succeeded', (e) => {
-    const { response } = e.detail;
-    const hasActivities = response?.execute?.pageLoad?.options?.length > 0;
-    console.log(hasActivities);
-  });
-
   return new Promise((resolve) => {
+    document.addEventListener('at-request-succeeded', (e) => {
+      const { response } = e.detail;
+      const hasActivities = response?.execute?.pageLoad?.options?.length > 0;
+      if (!hasActivities) resolve();
+    });
+
+
     document.addEventListener(LOAD_EVENT, () => {
       const markers = document.querySelectorAll('.at-element-marker');
       for (const marker of markers) {
