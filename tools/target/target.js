e266914823b42d5fe1dc55711883bc532a5b2022
Chris Millar <cmillar@adobe.com>
2026-03-08 18:15:46 -0600

    Clean up unneccessary selector regex

--- tools/target/target.js
+++ tools/target/target.js
@@ -1,11 +1,3 @@
-function convertTargetSelector(selector) {
-  // Converts :eq(0) to :nth-of-type(1), :eq(1) to :nth-of-type(2), etc.
-  return selector.replace(/:eq\((\d+)\)/g, (match, p1) => {
-    const index = parseInt(p1, 10) + 1;
-    return `:nth-of-type(${index})`;
-  });
-}
-
 const loadTargetOffers = async () => {
   // Look for offers
   const offers = await window.adobe.target.getOffers({
@@ -16,8 +8,7 @@ const loadTargetOffers = async () => {
   offers.execute.pageLoad.options.forEach((opt) => {
     const { cssSelector, content } = opt.content[0];
 
-    const selector = convertTargetSelector(cssSelector);
-    const el = document.querySelector(selector);
+    const el = document.querySelector(cssSelector);
 
     el.outerHTML = content;
   });
