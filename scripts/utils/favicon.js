81679b9afe8ec57a9efd6bad0c1ba80d316ce339
Chris Millar <chris@millr.org>
2026-03-22 22:14:50 -0600

    Remove duplicate favicon loading

--- scripts/utils/favicon.js
+++ scripts/utils/favicon.js
@@ -5,11 +5,11 @@ import { getConfig, getMetadata } from '../ak.js';
   const name = getMetadata('favicon') || 'favicon';
   const favBase = `${codeBase}/img/favicons/${name}`;
 
-  const tags = `<link rel="icon" href="${favBase}.svg" type="image/svg+xml">
-                <link rel="apple-touch-icon" href="${favBase}-180.png">
+  // Load before setting the main icon to prevent icon re-evaluation
+  const tags = `<link rel="apple-touch-icon" href="${favBase}-180.png">
                 <link rel="manifest" href="${favBase}.webmanifest">`;
-
   document.head.insertAdjacentHTML('beforeend', tags);
+
   const favicon = document.head.querySelector('link[href="data:,"]');
   if (favicon) favicon.href = `${favBase}.ico`;
 }());
